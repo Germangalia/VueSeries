9a4a406827835dc8979a26fc5808f93ed0fb569a
Germangalia <germangalia@iesebre.com>
2015-11-05 16:37:50 +0100

    Exercici video VUE versió 3

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ new Vue({
     el: '#tasks',
 
     data: {
-        tasks: ['one', 'two', 'three']
+        tasks: [
+
+            { body: 'Go to the store', completed: false}
+        ],
+        newTask: 'foobar'
+    },
+
+    methods: {
+        addTask: function() {
+            alert(this.newTask);
+
+        }
     }
 })
